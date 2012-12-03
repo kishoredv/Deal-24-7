321fb3c3603611bf31f8dbf0803360fee69c4a78
KishoreNCI <dvkisho@gmail.com>
2012-12-03 09:56:03 +0000

    Integration of API's in Web Application

---
+++ deal/deals/app/models/deal.rb
@@ -0,0 +1,4 @@
+class Deal < ActiveRecord::Base
+
+  attr_accessible :name
+end
---
+++ deal/deals/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deals::Application.config.secret_token = 'c61c706eae959e979b52276a2261df04dceba87fa837d70e07ebfffa10448fa4bb0f572cadd119cf50e66e9f3ee1b2f4706a676f720f9d28b6d521718a43751e'
